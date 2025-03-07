55192ab847acd8d8bf5db74fde2ba82a6af4855e
Gabriel Alves <125412432+Notkinn@users.noreply.github.com>
2025-03-07 18:54:11 -0300

    Update 10.kt

--- 10.kt
+++ 10.kt
@@ -1,12 +1,14 @@
 //10 - Escreva um programa para imprimir todas as tabuadas de 1 a N. N será informado pelo usuário.
 fun main(){
-    var numero: Int]
-    var cont = 1
+   var numero: Int
+   var cont = 1
 
     print("Digite um numero inteiro: ")
     numero = readln().toInt()
 
+    
   while (cont <= numero){
+
     var numero3 = 1
       while (numero3 <= 10){
           println("$cont X $numero3 = ${cont*numero3}")
@@ -15,4 +17,5 @@ fun main(){
       println("______________________________")
       cont++
       }
+
   }
